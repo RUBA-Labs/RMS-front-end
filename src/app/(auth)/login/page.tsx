91b26aa8713afe2d38c5d34787198bdff9bf4e8f
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-08-20 00:04:45 +0530

    tight them input

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
 import { LoginForm } from "../component/login-form"
+import { ModeToggle } from "@/components/ModeToggle"
 
 export default function LoginPage() {
   return (
@@ -13,16 +14,12 @@ export default function LoginPage() {
         />
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
-          </a>
-        </div>
+        
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
+            <div className="absolute top-4 right-4">
+                <ModeToggle />
+              </div>
             <LoginForm />
           </div>
         </div>
