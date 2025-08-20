ddc2ad673d1d382141c4f83e4607d10bcb2ab5cc
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-08-20 15:50:02 +0530

    fix

--- src/app/(main)/(dashboard)/layout.tsx
+++ src/app/(main)/(dashboard)/layout.tsx
@@ -1,41 +1,4 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "./components/app-sidebar"
-import { DashboardHeader } from "./components/DashboardHeader"
-import { MainArea } from "./components/main-container"
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
 
-// //mens import area 
-// import { menuItemsAcademic } from "./(pages)/academic/sidebarLists"
-// import { menuItemsAdmin } from "./(pages)/admin/sidebarLists"
-
-
-const menuItemsAcademic = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-];
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   console.log(children)
