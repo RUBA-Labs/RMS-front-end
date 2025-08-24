55e1e89a09209a85861b286b0d0006bf0b4c1b7b
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-08-24 19:23:12 +0530

    Menus

--- src/app/(main)/(dashboard)/(pages)/academic/page.tsx
+++ src/app/(main)/(dashboard)/(pages)/academic/page.tsx
@@ -9,6 +9,11 @@ export default function Academic() {
    const menuItemsAcademic = [
     { title: "Home", url: "#", icon: "Home", content: "Home" },
     { title: "Inbox", url: "#", icon: "Inbox", content: "Inbox" },
+    { title: "Timetable", url: "#", icon: "Calendar", content: "Timetable" },
+    { title: "Results", url: "#", icon: "ClipboardList", content: "Results" },
+    { title: "Attendance", url: "#", icon: "CheckCircle", content: "Attendance" },
+    { title: "Exams", url: "#", icon: "BookOpen", content: "Exams" },
+    { title: "Academic Calendar", url: "#", icon: "CalendarDays", content: "Academic Calendar" }
   ];
 
   return (
