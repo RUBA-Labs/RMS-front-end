48648c550cef080c07f4b2cbea4440ad4d2191ba
Bhathiya Piyathissa <bhathiyathiwanka019@gmail.com>
2025-09-14 12:50:23 +0530

    MySchedule component

--- src/app/(main)/(dashboard)/(pages)/student/TabContents/MySchedule.tsx
+++ src/app/(main)/(dashboard)/(pages)/student/TabContents/MySchedule.tsx
@@ -1,6 +1,28 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
 export function MySchedule(){
     return(
         <>
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                             
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+
         <h1>Personalized schedule</h1>
         </>
     )
