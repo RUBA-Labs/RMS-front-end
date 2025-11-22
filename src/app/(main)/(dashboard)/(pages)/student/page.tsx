573175048228d9d9da596d88ff4a5909bdf6be31
Uditha Wijethunga <udithalakshan321@gmail.com>
2025-11-22 17:05:30 +0530

    student dashboard update
    student dashboard update

--- src/app/(main)/(dashboard)/(pages)/student/page.tsx
+++ src/app/(main)/(dashboard)/(pages)/student/page.tsx
@@ -5,14 +5,14 @@ import { Home } from "./TabContents/Home";
 import Profile from "@/app/(main)/profile/page";
 
 import { Resources } from "./TabContents/Resources";
-import  Timetable  from "./TabContents/StudentTimetable";
+import  StudentTimetable  from "./TabContents/StudentTimetable";
+import TimeTableClash from "./TabContents/TimeTableClash";
 
 export default function Student() {
     const menuItemsStudent = [
         { title: "Home", url: "#", icon: "Home", content: "Home" },
-        { title: "Profile", url: "#", icon: "UserRound", content: "Profile" },
         { title: "Timetable", url: "#", icon: "Calendar", content: "Timetable" },
-        { title: "My Courses", url: "#", icon: "BookOpenCheck", content: "My Courses" },
+        { title: "TimeTableClash", url: "#", icon: "NewClaim", content: "TimeTableClash" },
         { title: "Resources", url: "#", icon: "Boxes", content: "Resources" }
     ];
 
@@ -21,7 +21,8 @@ export default function Student() {
             <div title="Home"><Home /></div>
             <div title="Profile"><Profile /></div>
             <div title="Resources"><Resources /></div>
-            <div title="Timetable"><Timetable /></div>
+            <div title="Timetable"><StudentTimetable /></div>
+            <div title="TimeTableClash"><TimeTableClash /></div>
         </DashboardPageProviders>
     );
 }
